14f21fe44771ae37e8e0d8330f54b05ef8d3006f
igara4 <fifty.storm.snowman@gmail.com>
2025-08-02 08:12:02 +0900

    chore:コーディング練習

---
+++ app/lib/practice.ts
@@ -0,0 +1,10 @@
+export function ensurePrefix(prefix: string, str: string) {
+  if (!str.startsWith(prefix)) {
+    return str + prefix;
+  }
+  return str;
+}
+
+export const ensurePrefix2 = (prefix: string, str: string): string => {
+  return str.startsWith(prefix) ? str : str + prefix;
+};
