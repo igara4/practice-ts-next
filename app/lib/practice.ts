87d9a9a633885adf3476ba4d465c050301fb9a10
igara4 <fifty.storm.snowman@gmail.com>
2025-08-04 07:51:40 +0900

    chore:コーディング練習コード修正

--- app/lib/practice.ts
+++ app/lib/practice.ts
@@ -33,3 +33,9 @@ export function toArray<T>(array?: Nullable<Arrayable<T>>) {
   array = array ?? [];
   return Array.isArray(array) ? array : [array];
 }
+
+export function flattenArrayable<T>(
+  array?: Nullable<Arrayable<T | Array<T>>>
+): Array<T> {
+  return toArray(array).flat(1) as Array<T>;
+}
