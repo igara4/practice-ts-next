c622e7d5d6ee7e5e8f74e4d89369c8fe4f353c28
igara4 <fifty.storm.snowman@gmail.com>
2025-09-29 07:25:58 +0900

    chore:コーディング練習　合計

--- app/lib/practice.ts
+++ app/lib/practice.ts
@@ -716,3 +716,16 @@ export const reverseString4 = (str: string) => {
   }
   return reversed;
 };
+
+const sumArray3 = (arr: number[]) => {
+  if (arr.length === 0) return 0;
+  return arr.reduce((sum, element) => sum + element, 0);
+};
+
+const sumArray4 = (arr: number[]) => {
+  let sum = 0;
+  for (const n of arr) {
+    sum += n;
+  }
+  return sum;
+};
