0aa218535d957f3571f47c253361769249dd3c60
igara4 <fifty.storm.snowman@gmail.com>
2025-10-08 07:30:23 +0900

    chore:コーディング練習　2つの数の和

--- app/lib/practice.ts
+++ app/lib/practice.ts
@@ -828,3 +828,14 @@ export const reverseEachWord3 = (str: string) => {
     .map((word) => word.split("").reverse().join(""))
     .join(" ");
 };
+
+export const twoSum2 = (arr: number[], target: number) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+  return null;
+};
